99aef7f74265703c582d10ebad277d5614ef769a
R. Biswas <rbiswas4@gmail.com>
2015-07-15 22:19:03 -0700

    Fixed data load problem

--- examples/example_read.py
+++ examples/example_read.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import analyzeSN as anS
 
-print os.listdir('example_data')
+example_data =  os.path.join(os.path.split(anS.__file__)[0], 'example_data')
+os.listdir(example_data)
--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ setup(# package information
       long_description=''' ''',
       # What code to include as packages
       packages=['analyzeSN'],
+      package_dir={'analyzeSN':'analyzeSN'},
       # What data to include as packages
       include_package_data=True,
-      package_data={'': ['example_data/*.FITS']}
-
+      package_data={'analyzeSN': ['example_data/*.FITS']}
       )
