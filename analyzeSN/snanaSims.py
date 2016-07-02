573dd475cc5f49960c78afe3ad3466391dfa6392
R. Biswas <rbiswas4@gmail.com>
2016-07-01 23:55:48 -0700

    light curves 	modified:   snanaSims.py

--- analyzeSN/snanaSims.py
+++ analyzeSN/snanaSims.py
@@ -5,7 +5,11 @@ A module for helper functions to read SNANA simulations
 """
 from __future__ import absolute_import
 import numpy as np
+from astropy.io import fits
+from astropy.utils import lazyproperty
+from astropy.table import Table
 import sncosmo
+import pandas as pd
 
 from . import filters
 
@@ -27,30 +31,47 @@ class SnanaSims(object):
 
     """
 
-    def __init__(self, headfile, photfile, snids=None, n=None):
+    def __init__(self, headfile, photfile, snids=None, n=None, cutFunc=None):
         """
         Instantiate class from SNANA simulation output files in fits format
 
         Parameters
         ----------
+        headfile : string, mandatory
+            absolute path to the 'HEAD' file of the simulation
+        photfile : string, mandatory
+            absolute path to the 'HEAD' file of the simulation
         snids : integer/string, optional defaults to None
             if not None, only SN observations corresponding to SNID snid
             are loaded
         n : Integer, defaults to None
             if not None, only the first n SN light curves are loaded
 
+        cutFunc : callable, optional, defaults to None
+           function of the form cutFunc(snid, headfile, photfile) = True if the
+           SN reperesented by snid in the files should be used. If None, no
+           function is applied and consequently all SN in the simulation are
+           used
 
         ..note: The column names of the SNANA data files are not reformated
                  for SNCosmo use
 
 
         """
-        self.snList = sncosmo.read_snana_fits(head_file=headfile,
-                                              phot_file=photfile,
-                                              snids=snids, n=None)
+        self.headfile = headfile
+        self.headdata = self.get_headData(self.headfile)
+        self.photfile = photfile
+        self.phothdu = fits.open(photfile)
+
+        self.snList = None # sncosmo.read_snana_fits(head_file=self.headfile,
+        #  phot_file=self.photfile,
+        #  snids=snids, n=None)
 
     @classmethod
-    def fromSNANAfileroot(cls, snanafileroot, location='./', snids=None,
+    def fromSNANAfileroot(cls,
+                          snanafileroot,
+                          location='./',
+                          snids=None,
                           n=None):
         """
         Class constructor from a root file and a location
@@ -60,7 +81,7 @@ class SnanaSims(object):
         snanafileroot : string, mandatory
             root file name for the SNANA which is the prefix to
             '_HEAD.FITS', or '_PHOT.FITS'
-        location : string, optional defaults to current working directory './' 
+        location : string, optional defaults to current working directory './'
             Relative or absolute path to the directory where the head and phot
             files are located
         snids : integer/string, optional defaults to None
@@ -74,16 +95,72 @@ class SnanaSims(object):
                                      location=location)
         photfile = cls.snanadatafile(snanafileroot, filetype='phot',
                                      location=location)
-        print headfile
-        data = sncosmo.read_snana_fits(head_file=headfile,
-                                       phot_file=photfile,
-                                       snids=snids, n=None)
+        # data = sncosmo.read_snana_fits(head_file=headfile,
+        #                               phot_file=photfile,
+        #                               snids=snids, n=None)
         return cls(headfile=headfile, photfile=photfile, snids=snids,
                    n=n)
 
     @staticmethod
+    def get_headData(headfile):
+        """
+        obtain the data in a headfile in a `pandas.DataFrame`
+
+        Parameters
+        ---------
+        headfile : string, mandatory
+            absolute path to an SNANA headFile
+
+        Returns
+        -------
+        A dataFrame with the data in the Head file, 
+        """
+        headData = Table.read(headfile).to_pandas()
+        # If the type is string (object in pandas)
+        if headData.SNID.dtype == 'O':
+            headData.SNID = headData.SNID.apply(lambda x: x.strip())
+        return headData
+
+
+
+    @staticmethod
+    def snlc(snid, headData, phothdu):
+        """
+        obtain the SN light curve from the photometry files
+
+        Parameters
+        ----------
+        headData : `panda.DataFrame`, mandatory
+        phothdu : `hdu` 
+        snid : index for SN. Can be integer or string.
+
+        Returns
+        -------
+        `numpy.recarray` of data
+        """
+        if not isinstance(snid, int):
+            # snid is a string
+            snid = snid.strip().lower()
+        
+        ptrs_cols = ['PTROBS_MIN', 'PTROBS_MAX']
+        ptrs = headData.query('SNID == @snid')[ptrs_cols].values
+        ptrs[0] -= 1
+
+        data = phothdu[1].data
+        dt = data.dtype
+
+        lc = []
+        for i, datum in enumerate(data):
+            if i >= ptrs[0]:
+                lc.append(datum)
+                if i > ptrs[1]:
+                    break
+        return np.array(lc, dtype=dt)
+
+
+    @staticmethod
     def snanadatafile(snanafileroot, filetype='head', location='./'):
-        '''
+        """
         obtain the name of the head or phot file of an SNANA simulation
         and dataset
 
@@ -103,7 +180,7 @@ class SnanaSims(object):
         -------
             string : absolute path to the SNANA file 
 
-        '''
+        """
         import os
 
         desiredfiletype = ['head', 'phot']
